0d2f1a5a164cb79a0b1ab48f1ed8ee5db2c4b713
Ryan C. Gordon <icculus@icculus.org>
2020-01-29 18:20:06 -0500

    Get rid of the bool/true/false redefines in log.h
    That was clearly never a good idea.

--- src/log.h
+++ src/log.h
@@ -93,9 +93,9 @@ ENDFUNCAS_SAFE(HANDLE, unsigned long, GetNextHandle());
 #include <cstdlib>
 #endif
 
-#define bool  int
-#define true  1
-#define false 0
+//#define bool  int
+//#define true  1
+//#define false 0
 
 #if !defined(NO_NEW_CPP_FEATURES)
 using namespace std;
